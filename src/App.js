9dc7c4bfdad6de3fbfd53b502a2ccf871c52cc22
Mayra Aguirre <125934783+MayAguirre25@users.noreply.github.com>
2023-08-24 20:37:04 -0500

    iniciando repositorio tienda

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import NavMenu from './components/header/NavMenu';
+import Store from './components/header/home/Store';
+
+function App() {
+  return (
+    <>
+      <NavMenu/>
+      <Store/>
+    </>
+
+
+  );
+}
+
+export default App;
